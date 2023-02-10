97908962707bd69ca93564e17acc4ae69524b3fd
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-10 09:55:46 -0500

    Completed most of conversion to react

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 export default function Features() {
     return (
-        <footer className="footer">
-            <br />
-            <p className="copyright">Copyright 2023 T26</p>
-        </footer>
+        <>
+            <div className="main">
+  <h1 className="features">Features</h1>
+  <br />
+  <p className="features-info">Online tutoring web has tons of features people can expect with modern online tutoring platform</p>
+  <br />
+  <h2 className="whiteboards">Whiteboards</h2>
+  <p className="whiteboard-info">Give you extremely good ideas and concepts needed to succeed well in your post secondary career.</p>
+  <br />
+  <h2 className="ontime-tutors">On time Tutors</h2>
+  <br />
+  <p className="ontime-tutors-info">Our tutors are expected to be focused and ontime while they work. If they do not help students as planned, we remove them</p>
+  <br />
+  <h2 className="feature-3">No additional price hikes</h2>
+  <br />
+  <p className="feature-3-info">As you are a student, our tutors get removed if they ask them to pay more or refuse to do work if you do not pay them. We know your tight budget very well</p>
+  <br />
+  <h2 className="final-feature">Strong expertise</h2>
+  <br />
+  <p className="final-info">Our tutors are only allowed to help students in their own expertise such as if you are a computer student, you get tutor who is expert in computers</p>
+</div>
+
+        </>
     )
 }
