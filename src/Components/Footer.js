3dc9c84e57c34fd2312ebf379a87dc1dc4cf24be
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-09 22:39:06 -0500

    fixed

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="by-footer">
+            <h6><span>© 2023</span> </h6>
+        </footer>
+    )
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <>
+            <div className="main-header"> <img src="Project Files/100-header.png" alt="main-header-2" width="100%" height={120} /></div>
+            <div className="flex-container">
+                <div className="about"> <img src="Project Files/about.png" alt="about" width={50} height={50} /> <a href="../about/about.html">About</a></div>
+                <div className="features"> <img src="Project Files/webcam.png" alt="webcam" width={50} height={50} /><a href="../features/features.html">Features</a></div>
+                <div className="pricing"> <img src="Project Files/best-price.png" alt="price" width={50} height={50} /><a href="../pricing/pricing.html">Pricing</a></div>  
+                <div className="benefits"> <img src="Project Files/benefits.png" alt="benefits" width={50} height={50} /><a href="#Benefits">Benefits</a></div>
+                <div className="becomeTutor"><img src="Project Files/tutorial.png" alt="tutorial" width={50} height={50} /><a href="#BecomeTutor">Become Tutor</a></div>
+                <div className="register"> <img src="Project Files/tutorial_logo.png" alt="register" width={50} height={50} /><a href="#register">Register</a></div>
+                <div className="login"> <img src="Project Files/login.png" alt="login" width={50} height={50} /><a href="#login">Login</a></div>
+            </div>
+        </>
+    )
+}
