46697c9c89e9fc79cfda1bb7b02cb6bd3469cd60
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-10 10:57:56 -0500

    Implemented css for everything

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export default function Features() {
     return (
         <>
@@ -8,20 +9,20 @@ export default function Features() {
   <br />
   <p className="features-info">Online tutoring web has tons of features people can expect with modern online tutoring platform</p>
   <br />
-  <h2 className="whiteboards">Whiteboards</h2>
-  <p className="whiteboard-info">Give you extremely good ideas and concepts needed to succeed well in your post secondary career.</p>
+  <h2 className="features-main">Whiteboards</h2>
+  <p className="features-info">Give you extremely good ideas and concepts needed to succeed well in your post secondary career.</p>
   <br />
-  <h2 className="ontime-tutors">On time Tutors</h2>
+  <h2 className="features-main">On time Tutors</h2>
   <br />
-  <p className="ontime-tutors-info">Our tutors are expected to be focused and ontime while they work. If they do not help students as planned, we remove them</p>
+  <p className="features-info">Our tutors are expected to be focused and ontime while they work. If they do not help students as planned, we remove them</p>
   <br />
-  <h2 className="feature-3">No additional price hikes</h2>
+  <h2 className="features-main">No additional price hikes</h2>
   <br />
-  <p className="feature-3-info">As you are a student, our tutors get removed if they ask them to pay more or refuse to do work if you do not pay them. We know your tight budget very well</p>
+  <p className="features-info">As you are a student, our tutors get removed if they ask them to pay more or refuse to do work if you do not pay them. We know your tight budget very well</p>
   <br />
-  <h2 className="final-feature">Strong expertise</h2>
+  <h2 className="features-main">Strong expertise</h2>
   <br />
-  <p className="final-info">Our tutors are only allowed to help students in their own expertise such as if you are a computer student, you get tutor who is expert in computers</p>
+  <p className="features-info">Our tutors are only allowed to help students in their own expertise such as if you are a computer student, you get tutor who is expert in computers</p>
 </div>
 
         </>
--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -3,24 +3,24 @@ import React from 'react';
 export default function Pricing() {
     return (
         <div className="main">
-  <h1>Pricing</h1>
-  <p>Our products are most affortable for our students</p>
+  <h1 className='pricing'>Pricing</h1>
+  <p className='pricing-info'>Our products are most affortable for our students</p>
   <br />
-  <p>We strive to keep our prices as low as possible for our students wih $5/m. 
+  <p className='pricing-info'>We strive to keep our prices as low as possible for our students wih $5/m. 
     <br />
   </p><table className="price">
     <thead className="price-head">
-      <tr><th>Type</th>
+      <tr className='price-head-row'><th className='price-head-data'>Type</th>
         <th>Price</th>
       </tr></thead>
     <tbody className="price-info">
       <tr>
-        <td>Student</td>
-        <td>$5.00/m</td>
+        <td className='pricing-data'>Student</td>
+        <td className='pricing-cost'>$5.00/m</td>
       </tr>
       <tr>
-        <td>Other</td>
-        <td>$60.00/m</td>
+        <td className='pricing-data'>Other</td>
+        <td className='pricing-cost'>$60.00/m</td>
       </tr>
     </tbody>
   </table>
