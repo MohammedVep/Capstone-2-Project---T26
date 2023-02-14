2db53eface38d9f7be45ed44c64e63ae6ad2e747
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-14 14:25:32 -0500

    Got building blocks of react and node together

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require("exprress")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 
+
 const io = require("socket.io")(server, {
     cors: {
         origin: "*",
@@ -10,3 +11,27 @@ const io = require("socket.io")(server, {
 });
 
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req,res) => {
+    res.send("Server is running.")
+});
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("calluser", { signal: signalData, from, name});
+    });
+
+    socket.on("answercall", (data) => {
+        io.to(data.io).emit("callaccepted", data.signal);
+    })
+});
+
+server.listen(PORT, () => console.log(`Server listening on ${PORT}`))
