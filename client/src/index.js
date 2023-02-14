07df6b6041316a28989ef584b05b0ba088d6d790
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-14 14:38:24 -0500

    work just started on chat application

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,6 @@
-import react from 'react'
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import './styles.css';
+
+ReactDOM.render(<App />, document.getElementsById('root'));
