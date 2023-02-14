c134fb5d6d02821759f0750aa6da7c00de17f687
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-14 11:38:22 -0500

    Completed Chat App

--- src/components/GetHelp.js
+++ src/components/GetHelp.js
@@ -5,22 +5,27 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import ChatApp from "./ChatApp";
 
 export default function GetHelp() {
     return (
         <>
+        <Router>
         <h1 className="page-title">Get Help</h1>
         <br />
         <div className="component">
           <table className="table">
             <tbody className="tutor-list">
                 <tr>
-                    <td></td>
+                    <td><Link to="/ChatApp">Tutor Name</Link></td>
                 </tr>
             </tbody>
           </table>
         </div>
-        
+        <Switch>
+            <Route path="/ChatApp"><ChatApp /></Route>
+        </Switch>
+        </Router>
         </>
     )
 }
