b1e76cec095ec7aed107a0437f4f5713d6a2d202
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-15 13:00:02 -0500

    updated backend and Modal.js

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -4,9 +4,23 @@ const socket = socketIO.connect("http://localhost:4000");
 
 const Modal = ({url}) => {
     const [image, setImage] = useState("");
+    const [fullHeight, setFullHeight] = useState("")
+
+    useEffect(() => {
+        socket.emit("browse", {
+            url,
+        });
+
+        socket.on("image", ({img, fullHeight }) => {
+            setImage("data:image/jpeg;base64," + img);
+            setFullHeight(fullHeight);
+        });
+    }, [url]);
     return (
         <div className='popup'>
-            <div className='popup-ref'>{image && <img src={image} alt='' />}</div>
+            <div className='popup-ref' style={{ height: fullHeight }}>
+            {image && <img src={image} alt='' />}
+            </div>
         </div>
     )
 }
