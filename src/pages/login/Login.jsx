089c3b07d826aabc5f8160a09bc468a5980c94e6
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-16 12:31:02 -0500

    completed UI for blog

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import './login.css'
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
@@ -10,8 +11,11 @@ export default function Login() {
             <label>Password</label>
             <input type="password" className='loginInput' placeholder='Enter password' />
             <button className="loginButton">Login</button>
-            <button className="loginRegisterButton">Register</button>
+            
         </form>
+        <button className="loginRegisterButton">
+            <Link className='link' to="/register">Register</Link>
+        </button>
     </div>
   )
 }
