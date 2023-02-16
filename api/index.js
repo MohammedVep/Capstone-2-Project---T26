85be9518967ea672d2e6fd07d491bb4a1ad5121a
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-16 17:39:28 -0500

    Getting started with back end components

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+const dotemv = require('dotemv');
+const mongoose = require('mongoose');
+
+dotemv.config()
+
+mongoose.connect(process.eventNames.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
+
+app.use("/", (req,res) =>{
+    console.log("Running");
+})
 
 app.listen("3000", () => {
     console.log("Backend running");
