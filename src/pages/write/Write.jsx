ea33d2b9ae9562f437dfbb20e6937ad68862a2a8
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-15 21:22:18 -0500

    Almost complete with UI

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -3,7 +3,24 @@ import './write.css'
 export default function Write() {
   return (
     <div className='write'>
-        <form className='writeForm'></form>
+        <img src="https://www.pexels.com/photo/photo-of-cloudy-sky-during-golden-hour-14677839/" alt="" className='writeImg'/>
+        <form className='writeForm'>
+            <div className="writeFormGroup">
+                <label htmlFor='fileInput'>
+                    <i className="writeIcon fa-solid fa-plus"></i>
+                </label>
+                <input type="file" id='fileInput' style={{display:"none"}} />
+                <input type="text" placeholder="Title" className='writeInput' autoFocus={true} />
+            </div>
+            <div className="writeFormGroup">
+                <textarea 
+                    placeholder='Tell your story...'
+                    type='text'
+                    className='writeInput writeText'
+                ></textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
+        </form>
     </div>
   )
 }
