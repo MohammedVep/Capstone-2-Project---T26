dac2c20c2ebddbb3b1e138f2e4d1003600660e58
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-16 20:36:01 -0500

    completed Backend

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const dotemv = require('dotemv');
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const categoriesRoute = require('./routes/categories');
+const multer = require("multer");
 
 dotemv.config()
 app.use(express.json());
@@ -15,12 +18,24 @@ mongoose.connect(process.eventNames.MONGO_URL, {
 }).then(console.log("Connected to MongoDB"))
 .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null, "images")
+    },filename:(req,file,cb) =>{
+        cb(null, req.body.name);
+    }
+});
+
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"),(req, res) => {
+    res.status(200).json("file uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoriesRoute);
 
-app.use("/", (req,res) =>{
-    console.log("Running");
-})
 
 app.listen("3000", () => {
     console.log("Backend running");
