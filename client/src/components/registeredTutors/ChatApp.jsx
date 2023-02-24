ceeda0b259b52d1ba55793b6f52e652701c7cb64
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-02-24 09:39:39 -0500

    Added chat components, will begin testing work soon

--- client/src/components/registeredTutors/ChatApp.jsx
+++ client/src/components/registeredTutors/ChatApp.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { ChatEngine } from 'react-chat-engine';
+import '../../App.css'
+
+const projectID = '634eeb78-d871-4ccf-b14c-1c064503001a';
 
 const ChatApp = () => {
   return (
-    <div>ChatApp</div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   )
 }
 
--- client/src/components/registeredUsers/ChatApp.jsx
+++ client/src/components/registeredUsers/ChatApp.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { ChatEngine } from 'react-chat-engine';
+import '../../App.css'
 
 const ChatApp = () => {
   return (
-    <div>ChatApp</div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   )
 }
 
