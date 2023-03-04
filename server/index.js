4c72bcda551010d7822b107de611509d12e7ac97
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-03-03 19:57:17 -0500

    Package started

--- server/index.js
+++ server/index.js
@@ -1,10 +1,36 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose;';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import studentsRoutes from './routes/student.js';
+
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cors());
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/students", studentsRoutes);
+
+const PORT = process.env.PORT || 9000;
+mongoose.
+    connect('mongodb+srv://Team26:pwercfLdwe24=vN$@@cluster0.fm45zrm.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
