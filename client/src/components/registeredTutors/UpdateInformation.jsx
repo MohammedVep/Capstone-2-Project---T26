31ebf51bc6afa0d09292baf213f714fb8882968b
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-04-11 07:51:03 -0400

    fixed

--- client/src/components/registeredTutors/UpdateInformation.jsx
+++ client/src/components/registeredTutors/UpdateInformation.jsx
@@ -1,61 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TutorUpdateInformation.css"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const UpdateInformation = () => {
-    const[value, setValue] = React.useState('province');
-    const handleChange = (event) => {
-        setValue(event.target.value);
+  const [user, setUser] = useState()
+  const handleChange = ({ currentTarget: input }) => {
+    setUser({ ...user, [input.name]: input.value });
+  };
+
+  const handleFormSubmit = async (ev) => {
+    ev.preventDefault();
+    console.log("update")
+    try {
+      const url = "https://t26-server.herokuapp.com/api/tutor/update";
+      const { data: res } = await axios.post(url, user);
+      navigate("/TutorMainMenu");
+
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status < 500
+      ) {
+        setErrors(error.response.data);
+        console.log(error.response);
+      }
     }
+  };
+
   return (
     <>
-        <h1 className="page-title">Update Information</h1>
-            <br />
-            <div className="component">
-              <form>
-                <label htmlFor="fname" className="form-name">First Name: </label>
-                <input type="text" id="fname" name="fname" />
+      <h1 className="page-title">Update Information</h1>
+      <br />
+      <div className="component">
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="fname" className="form-name">First Name: </label>
+          <input type="text" id="fname" name="fname" onChange={handleChange} />
 
-                <label htmlFor="lname" className="form-name">Last Name: </label>
-                <input type="text" id="lname" name="lname" />
+          <label htmlFor="lname" className="form-name">Last Name: </label>
+          <input type="text" id="lname" name="lname" onChange={handleChange} />
 
-                <label htmlFor="address" className="form-name">Address: </label>
-                <input type="text" id="address" name="address" />
+          <label htmlFor="address" className="form-name">Address: </label>
+          <input type="text" id="address" name="address" onChange={handleChange} />
 
-                <label htmlFor="city" className="form-name">City: </label>
-                <input type="text" id="city" name="city" />
+          <label htmlFor="city" className="form-name">City: </label>
+          <input type="text" id="city" name="city" onChange={handleChange} />
 
-                <label htmlFor="province" className="form-name">Province: </label>
-                <input type="text" id="province" name="province" />
+          <label htmlFor="province" className="form-name">Province: </label>
+          <input type="text" id="province" name="province" onChange={handleChange} />
 
-                <label htmlFor="pcode" className="form-name">Postal Code: </label>
-                <input type="text" id="pcode" name="pcode" />
+          <label htmlFor="pcode" className="form-name">Postal Code: </label>
+          <input type="text" id="pcode" name="pcode" onChange={handleChange} />
 
-                <label htmlFor="country" className="form-name">Country: </label>
-                <input type="text" id="country" name="country" />
+          <label htmlFor="country" className="form-name">Country: </label>
+          <input type="text" id="country" name="country" onChange={handleChange} />
 
-                <label htmlFor="email" className="form-name">Email: </label>
-                <input type="email" id="email" name="email" />
+          <label htmlFor="email" className="form-name">Email: </label>
+          <input type="email" id="email" name="email" onChange={handleChange} />
 
-                <label htmlFor="password" className="form-name">Password: </label>
-                <input type="password" id="password" name="password" />
+          <label htmlFor="password" className="form-name">Password: </label>
+          <input type="password" id="password" name="password" onChange={handleChange} />
 
-                <label htmlFor="cpassword" className="form-name">Confirm Password: </label>
-                <input type="password" id="cpassword" name="cpassword" />
+          <label htmlFor="cpassword" className="form-name">Confirm Password: </label>
+          <input type="password" id="cpassword" name="cpassword" onChange={handleChange} />
 
-                <label htmlFor="transitN" className="form-name">Transit Number: </label>
-                <input type="text" id="transitN" name="transitN" />
+          <label htmlFor="transitN" className="form-name">Transit Number: </label>
+          <input type="text" id="transitN" name="transitN" onChange={handleChange} />
 
-                <label htmlFor="accN" className="form-name">Account Number: </label>
-                <input type="text" id="accN" name="accN" />
+          <label htmlFor="accN" className="form-name">Account Number: </label>
+          <input type="text" id="accN" name="accN" onChange={handleChange} />
 
-                <label htmlFor="branchN" className="form-name">Branch Number: </label>
-                <input type="text" id="branchN" name="branchN" />
+          <label htmlFor="branchN" className="form-name">Branch Number: </label>
+          <input type="text" id="branchN" name="branchN" onChange={handleChange} />
 
-                <input className="btn btn-primary" type="submit" defaultValue="Update info" />
-                  <a className="btn btn-primary" role="button"><Link to="/TutorMainMenu">Cancel</Link></a>
-              </form>
-            </div>
+          <input className="btn btn-primary" type="submit" defaultValue="Update info" />
+          <a className="btn btn-primary" role="button"><Link to="/TutorMainMenu">Cancel</Link></a>
+        </form>
+      </div>
     </>
   )
 }
